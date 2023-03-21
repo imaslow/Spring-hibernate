92e6cf258c2aa68a853c7712c6ad877bbf397e36
Ilya <127244460+imaslow@users.noreply.github.com>
2023-03-21 22:21:47 +0300

    Add files via upload

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,46 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "series")
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+
+    @Override
+    public String toString() {
+        return model + ' ' + series;
+    }
+}
